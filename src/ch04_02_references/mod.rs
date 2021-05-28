13caec4bbb23fbce627a755559b0aa5eacde0c2f
Alex Black <wa7@sa34.cx>
2021-05-28 12:55:33 +0300

    References

---
+++ src/ch04_02_references/mod.rs
@@ -0,0 +1,51 @@
+pub fn run() {
+    let s = String::from("hello");
+    let length = calc_length(&s);
+    println!("The length of `{}` is {}", s, length);
+
+    let length = s.len();
+    println!("The length of `{}` is {}", s, length);
+
+    // So what happens if we try to modify something we’re borrowing?
+    // It doesn’t work!
+    // change(&s);
+
+    let mut s = String::from("It's a party");
+    change(&mut s);
+    println!("{}", s);
+
+    /*
+    * We also cannot have a mutable reference while we have an immutable one
+    */
+    let s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    // let r3 = &mut s; // BIG PROBLEM
+
+    // println!("{}, {}, and {}", r1, r2, r3);
+    println!("{} and {}", r1, r2);
+
+    /*
+    * Dangling References
+    */ 
+    // let ref_to_nothing = dangle();
+
+}
+
+fn calc_length(s: &String) -> usize {
+  s.len()
+}
+
+// fn change(some_string: &String) {
+//     some_string.push_str(", world");
+// }
+
+fn change(some_string: &mut String) {
+    some_string.push_str(" time 🎉");
+}
+
+fn dangle() -> &String {
+  let s = String::from("hello");
+  &s
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![allow(dead_code, unused_imports)]
+
+mod ch04_01_ownership;
+mod ch04_02_references;
+
+use ch04_01_ownership as ownership;
+use ch04_02_references as references;
+
 fn main() {
-    println!("Hello, world!");
+    // ownership::run();
+    references::run();
+    // color::run();
 }
