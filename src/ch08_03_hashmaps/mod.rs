9f8214991e6451bedcb2eadf9b476ea26802f14b
Alex Black <wa7@sa34.cx>
2021-06-10 13:27:34 +0300

    Hashmap

---
+++ src/ch08_03_hashmaps/mod.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+pub fn run() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("blue"), 20);
+    scores.insert(String::from("red"), 50);
+
+    let mut my_hash = HashMap::new();
+    my_hash.insert("blue", 50);
+    my_hash.insert("orange", 20);
+
+    println!("{:?}", my_hash);
+
+    println!("The score of `blue` is {:?}", my_hash.get("blue").unwrap());
+    
+    /*
+    * Implementations
+    */
+
+    // Explore this later ...
+}
